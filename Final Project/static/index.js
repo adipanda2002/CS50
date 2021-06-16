28f8eaf4aeacf457bb63aab91523537d397b377f
adipanda2002 <75654476+adipanda2002@users.noreply.github.com>
2021-06-16 18:47:36 +0800

    Create index.js

---
+++ Final Project/static/index.js
@@ -0,0 +1,8 @@
+  function deleteReflection(reflectionId) {
+  fetch("/delete-reflection", {
+    method: "POST",
+    body: JSON.stringify({ reflectionId: reflectionId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
